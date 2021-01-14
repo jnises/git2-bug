fb8c9f31e9b3992088f4addae96f969353e15f66
Joel Nises <joel.nises@gmail.com>
2021-01-14 21:21:09 +0100

    added code that triggers segfault

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let r = git2::Repository::open(".").unwrap();
+    let b = r.blame_file(std::path::Path::new("README.md"), None).unwrap();
+    let h = b.get_index(0).unwrap();
+    let s = h.final_signature();
+    let _boom = s.to_string();
 }
